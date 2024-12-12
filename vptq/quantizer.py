da1ce234736f1875358f070ef04df6d5a7f97130
Julien Balian <julien.balian@sonos.com>
2024-12-12 11:29:05 +0100

    fix: cap

--- vptq/quantizer.py
+++ vptq/quantizer.py
@@ -411,6 +411,9 @@ class NPVectorQuantizer:
                 vector_weights, _ = self.res_reshaper[idx].matrix2vectors(train_weights)
 
                 # kmean
+                new_num_centroid = min(vector_weights.shape[0], num_centroids)
+                if new_num_centroid != num_centroids:
+                    self.logger.info(f"requested num centroids: {num_centroids} over {new_num_centroid} samples")
                 _kmeans = self.kmeans_model(num_centroids)
 
 
@@ -419,6 +422,7 @@ class NPVectorQuantizer:
                 )
                 # self.logger.info(sub_vectors.shape)
                 sub_vectors = sub_vectors.to(torch.float32).cpu().numpy()
+                vector_weights = vector_weights.to(torch.float32).cpu().numpy()
                 _kmeans.fit(sub_vectors, sample_weight=vector_weights)
                 self.logger.info(
                     f"cuml kmeans {_kmeans.n_iter_} iterations, error {_kmeans.inertia_}"
